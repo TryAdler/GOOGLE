46a0168d6f38a3a18c37117c4b9a8fe1a4e0f834
Diagrasias <diagrasias.dev@gmail.com>
2023-10-01 11:32:43 +0300

    Update project2google.py

--- project2google.py
+++ project2google.py
@@ -1,6 +1,6 @@
 A = int(input('Введите любую цифру))
 if A >= 5:
-	print ('Hello, MIREA!')
+	print ('Hello, Adler!')
 elif A > 9 or A < 0:
 	print ('Это не цифра, запустите код снова')
 else: print ('Why?')
